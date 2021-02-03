ae58c7f4e82cc0fb21c3194da742af5407452392
Sanjay Pandey <vigyanhoon@yahoo.co.in>
2021-02-03 23:51:22 +0530

    add audio

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,9 +1,10 @@
-import { StackNavigationProp } from '@react-navigation/stack';
-import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
-
-import { RootStackParamList } from '../../App';
-import { Button } from 'react-native-paper';
+import {StackNavigationProp} from '@react-navigation/stack';
+import React, {useState} from 'react';
+import {StyleSheet, View, Text} from 'react-native';
+import {Audio} from 'expo-av'
+import {RootStackParamList} from '../../App';
+import {Button} from 'react-native-paper';
+var Sound = require('react-native-sound');
 
 type NavigationProp = StackNavigationProp<RootStackParamList, 'Home'>;
 
@@ -11,13 +12,43 @@ interface Props {
   navigation: NavigationProp;
 }
 
-const Home = ({ navigation }: Props): JSX.Element => {
+const Home = ({navigation}: Props): JSX.Element => {
+  const [audio, setAudio] = useState();
+
+  const play = async () => {
+    var whoosh = new Sound('malini.mp3', Sound.MAIN_BUNDLE, (error) => {
+      if (error) {
+        console.log('failed to load the sound', error);
+        return;
+      }
+      // loaded successfully
+      console.log('duration in seconds: ' + whoosh.getDuration() + 'number of channels: ' + whoosh.getNumberOfChannels());
+
+      // Play the sound with an onEnd callback
+      whoosh.play((success) => {
+        if (success) {
+          console.log('successfully finished playing');
+        } else {
+          console.log('playback failed due to audio decoding errors');
+        }
+      });
+    });
+    setAudio(whoosh)
+  }
+
+  const stop = () => {
+    audio.stop()
+  }
+
   return (
     <>
       <View style={styles.body}>
         <Text>Hi</Text>
-        <Button icon="camera" mode="contained" onPress={() => console.log('Pressed')}>
-          Press me
+        <Button icon="play" mode="contained" onPress={play}>
+          Play
+        </Button>
+        <Button icon="play" mode="contained" onPress={stop}>
+          Stop
         </Button>
       </View>
     </>
