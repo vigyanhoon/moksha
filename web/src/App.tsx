d1b916729e508a4f523427f33c933e08f3ac3166
vigyanhoon <vigyanhoon@gmail.com>
2022-06-13 15:11:29 +0530

    click to play

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,45 +1,33 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import './App.css';
-import {mantras, MantraType} from './mantras';
-import {createUseStyles} from 'react-jss'
+import { mantras, MantraType } from './mantras';
+import { createUseStyles } from 'react-jss'
 import mp3 from './vishnu_sahasranamam.mp3'
 
 const useStyles = createUseStyles({
-  body:{
-    backgroundColor:'#FFCC99'
+  body: {
+    backgroundColor: '#FFCC99'
   },
   mantra: {
     fontSize: '1.5em',
     padding: '20px 0',
     whiteSpace: 'pre-wrap',
     userSelect: 'none',
-    color: 'blue'
   }
 })
 
-let audio = new Audio(mp3)
+const audio = new Audio(mp3)
 
-const start = (time:number) => {
-  if(audio.paused){
-    audio.currentTime = time
-    audio.play()
-  } else {
-    audio.pause()
-  }
-  
-}
 
-const Mantra = ({mantra}: {mantra:MantraType})=>{
+const Mantra = ({ mantra, currentPlaying, index, play }: { mantra: MantraType, currentPlaying: number, index: number, play: (m: MantraType) => void }) => {
   const classes = useStyles()
   const m = mantra.mantra
   const parts = m.split('। ')
 
-  const selected = ()=> {
-    start(mantra.time)
-  }
+  const color = currentPlaying === index ? 'red' : ''
 
   return (
-    <div onClick={selected} className={`${classes.body} ${classes.mantra}`}>
+    <div onClick={() => play(mantra)} className={`${classes.body} ${classes.mantra}`} style={{ color }}>
       <div>{parts[0] + '।'}</div>
       <div>{parts[1]}</div>
       <div>{parts[2]}</div>
@@ -48,10 +36,36 @@ const Mantra = ({mantra}: {mantra:MantraType})=>{
 }
 
 function App() {
+  const [currentPlaying, setCurrentPlaying] = useState(0)
+  let index = useRef(0)
+
+  function handleEvent(e: Event) {
+    const nextTime = mantras[index.current + 1].time
+
+    if (audio.currentTime > nextTime) {
+      index.current++
+      setCurrentPlaying(index.current)
+    }
+  }
+
+  useEffect(() => {
+    audio.addEventListener('timeupdate', handleEvent);
+
+    return () => audio.removeEventListener('timeupdate', handleEvent);
+  }, [])
+
+  const play = (m: MantraType) => {
+    const i = mantras.indexOf(m)
+    index.current = i
+    setCurrentPlaying(i)
+    audio.currentTime = m.time
+    audio.play()
+  }
+
   return (
     <div className="App">
-      {mantras.map((m, i)=>(
-        <Mantra key={i} mantra={m}/>
+      {mantras.map((m, i) => (
+        <Mantra key={i} mantra={m} currentPlaying={currentPlaying} index={i} play={play} />
       ))}
     </div>
   );
