c4e9743940774344acc13ad6709c9c9142f1f2cd
vigyanhoon <vigyanhoon@gmail.com>
2023-04-12 08:36:01 +0530

    conditional config

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,8 +1,14 @@
-import { defineConfig } from 'vite'
+import { UserConfigExport, defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
+const isProd = process.env.NODE_ENV === 'production'
+
 // https://vitejs.dev/config/
-export default defineConfig({
+
+const config:UserConfigExport = {
   plugins: [react()],
-  base: 'Moksha'
-})
+}
+
+if(isProd) config.base = '/Moksha'
+
+export default defineConfig(config)
