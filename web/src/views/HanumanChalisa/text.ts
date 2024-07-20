e92d05888bf23a7d56a2387c50fc1a96b403d48d
vigyanhoon <vigyanhoon@gmail.com>
2024-07-20 16:15:36 +0530

    add english and meaning

--- web/src/views/HanumanChalisa/text.ts
+++ web/src/views/HanumanChalisa/text.ts
@@ -11,9 +11,19 @@ const texts: MantraType[] = [
     बल बुधि बिद्या देहु मोहिं
     हरहु कलेस बिकार ॥`,
     time: 2.6,
-    english: '',
+    english: `Shree Guru Charan Saroj Raj
+    Nij Man Mukur Sudhari ।
+    Barnau Raghuvar Bimal Jas
+    Jo dayak Phal Chari ॥
+    Budhi heen Tanu Janike
+    Sumirow Pavankumar ।
+    Bal Buddhi Vidya Dehu Mohi, 
+    Harahu Kalesh Bikaar ॥`,
     meaning:
-      'One should meditate for the removal of all obstacles upon Vishnu who is clad in white garments, who has a lustre like the moon, who has four arms and a beneficent face.',
+      `With the Dust of the Lotus Feet of Sri Gurudeva, I Clean the Mirror of my Mind.
+      I Narrate the Sacred Glory of Sri Raghubar (Sri Rama Chandra), who Bestows the Four Fruits of Life (Dharma, Artha, Kama and Moksha).
+      Considering Myself as Ignorant, I Meditate on You, O Pavan Kumar (Hanuman).
+      Bestow on me Strength, Wisdom and Knowledge, and Remove my Afflictions and Blemishes.`,
   },
   {
     mantra: `जय हनुमान ज्ञान-गुन-सागर ।
@@ -21,8 +31,12 @@ const texts: MantraType[] = [
     राम-दूत अतुलित-बल-धामा ।
     अंजनिपुत्र - पवनसुत - नामा ॥ २ ॥`,
     time: 22.7,
-    english: '',
-    meaning:'',
+    english: `Jai Hanuman Gyan Gun Sagar । Jai Kipis Tihun Lok Ujgaar
+    Ramdoot Atulit Bal Dhamaa । Anjani Putra Pavansut naamaa.`,
+    meaning: `Victory to You, O Hanuman, Who is the Ocean of Wisdom and Virtue,
+    Victory to the Lord of the Monkeys, Who is the Enlightener of the Three Worlds.
+    You are the Messenger of Sri Rama possessing Immeasurable Strength,
+    You are Known as Anjani-Putra (son of Anjani) and Pavana-Suta (son of Pavana, the wind-god).`,
   },
   {
     mantra: `महाबीर बिक्रम बजरंगी ।
@@ -30,17 +44,27 @@ const texts: MantraType[] = [
     कंचन-बरन बिराज सुबेसा ।
     कानन कुंडल कुंचित केसा ॥ ४ ॥`,
     time: 32.8,
-    english: '',
-    meaning:'',
+    english: `Mahabeer Bikram Bajrangi । Kumati Nivaar Sumati Ke Sangi
+    Kanchan Baran Biraaj Subesaa । Kanan kundal kunchit kesa`,
+    meaning: `You are a Great Hero, extremely Valiant, and body as strong as Thunderbolt,
+    You are the Dispeller of Evil Thoughts and Companion of Good Sense and Wisdom.
+    You possess a Golden Hue, and you are Neatly Dressed,
+    You wear Ear-Rings and have beautiful Curly Hair.
+    `,
   },
   {
     mantra: `हाथ बज्र अरु ध्वजा बिराजै ।
     काँधे मूँज-जनेऊ साजै॥ ५ ॥
-    शंकर स्वयं केसरीनंदन ।
+    शंकर-सुवन केशरी-नन्दन ।
     तेज प्रताप महा जग-बंदन ॥ ६ ॥`,
     time: 42.9,
-    english: '',
-    meaning:'',
+    english: `Hath Bajra Aur Dhvaja Birjai । Kandhe Moonj Janeu saage
+    Shankar Suvna Kesari Nandan । Tej Pratap Maha Jag Bandan`,
+    meaning: `You hold the Thunderbolt and the Flag in your Hands.
+    You wear the Sacred Thread across your Shoulder.
+    You are the Incarnation of Lord Shiva and Son of Kesari,
+    You are Adored by the whole World on account of your Great Strength and Courage.
+    `,
   },
   {
     mantra: `बिद्यावान गुनी अति चातुर ।
@@ -48,8 +72,13 @@ const texts: MantraType[] = [
     प्रभु-चरित्र सुनिबे को रसिया ।
     राम-लखन-सीता-मन-बसिया ॥ ८ ॥`,
     time: 53,
-    english: '',
-    meaning: '',
+    english: `Vidyavaan Guni Ati Chatur । Ram Kaj Karibe Ko Atur
+    Prabhu Charittra Sunibe Ko Rasiya । Ram Lakhan Sita man basyia`,
+    meaning:  `You are Learned, Virtuous and Extremely Intelligent,
+    You are always Eager to do the Works of Sri Rama.
+    You Delight in Listening to the Glories of Sri Rama,
+    You have Sri Rama, Sri Lakshmana and Devi Sita Dwelling in your Heart.
+    `,
   },
   {
     mantra: `सूक्ष्म रूप धरि सियहिं दिखावा ।
@@ -57,8 +86,13 @@ const texts: MantraType[] = [
     भीम रूप धरि असुर सँहारे ।
     रामचंद्र के काज सँवारे ॥ १० ॥`,
     time: 63.1,
-    english: '',
-    meaning: '',
+    english: `Sukshma roop Dhari Siyahi Dikhwana । Bikat roop Dhari Lank Jarawa
+    Bhim roop Dhari Asur Sanhare । Ramchandra Ke kaaj Savare`,
+    meaning:  `You Appeared before Devi Sita Assuming a Diminutive Form (in Lanka),
+    You Assumed an Awesome Form and Burnt Lanka.
+    You Assumed a Gigantic Form and Destroyed the Demons,
+    Thereby Accomplishing the Task of Sri Rama.
+    `,
   },
   {
     mantra: `लाय सँजीवनि लखन जियाये ।
@@ -66,8 +100,13 @@ const texts: MantraType[] = [
     रघुपति कीन्ही बहुत बड़ाई ।
     तुम मम प्रिय भरतहिं सम भाई ॥ १२ ॥`,
     time: 73.2,
-    english: '',
-    meaning: '',
+    english: `Laye Sajivan Lakhan Jiyaye । Shri Raghubir harashi ur laye
+    Raghupati Kinhi Bahut Badaai । Tum Mama Priya Bharat Sam Bahi.`,
+    meaning:  `You Brought the Sanjivana herb and Revived Sri Lakshmana.
+    Because of this Sri Rama Embraced You overflowing with Joy.
+    Sri Rama Praised You Greatly,
+    And said: "You are as dear to me as my brother Bharata".
+    `,
   },
   {
     mantra: `सहसबदन तुम्हरो जस गावैं ।
@@ -75,26 +114,40 @@ const texts: MantraType[] = [
     सनकादिक ब्रह्मादि मुनीसा।
     नारद सारद सहित अहीसा॥ १४ ॥`,
     time: 83.3,
-    english: '',
-    meaning: '',
+    english: `Sahastra Badan Tumharo Jas Gaave । Asa kahi Shripati Kanth Laagave.
+    Sankadik Brahmadi Muneesa । Narad Sarad Sahit Aheesa`,
+    meaning:  `The Thousand Headed Seshnag Sings Your Glory",
+    Said Sri Rama to You taking you in his Embrace
+    Sanaka and other Sages, Lord Brahma and other Gods,
+    Narada, Devi Saraswati and Seshnag ...
+    `,
   },
   {
-    mantra: `जम कुबेर दिगपाल जहाँ ते ।
+    mantra: `यम कुबेर दिगपाल जहाँते ।
     कबि कोबिद कहि सकैं कहाँ ते ॥ १५ ॥
     तुम उपकार सुग्रीवहिं कीन्हा ।
     राम मिलाय राज-पद दीन्हा ॥ १६ ॥`,
     time: 93.3,
-    english: '',
-    meaning: '',
+    english: `Jam Kuber Digpal Jahan Te । Kabi Kobid Kahin Sake Kahan Te
+    Tum Upkar Sugrivahi Keenha । Ram Milay Rajpad Deenha`,
+    meaning:  `Yama (god of death), Kubera (god of wealth), Digpalas (the guardian deities),
+    Poets and Scholars have not been able to Describe Your Glories in full.
+    You Rendered a great Help to Sugriva.
+    You Introduced him to Sri Rama and thereby Gave back his Kingdom.
+    `,
   },
   {
     mantra: `तुम्हरो मंत्र बिभीषन माना ।
     लंकेश्वर भए सब जग जाना ॥ १७ ॥
-    जुग सहस्र जोजन पर भानू ।
+    युग सहस्र योजन पर भानू ।
     लील्यो ताहि मधुर फल जानू ॥ १८ ॥`,
     time: 103.5,
-    english: '',
-    meaning: '',
+    english: `Tumharo Mantr Bibhishan Maana । Lankeshwar Bhaye Sab Jag Jaana
+    Jug Sahastra Jojan Par Bhaanu । Leelyo Taahi Madhur Phal Jaanu`,
+    meaning:  `Vibhisana Followed your Advice,
+    And the Whole World Knows that he became the King of Lanka.
+    The Sun which was at a distance of Sixteen Thousand Miles,
+    You Swallowed It (the Sun) thinking it to be a Sweet Fruit.`,
   },
   {
     mantra: `प्रभु-मुद्रिका मेलि मुख माहीं ।
@@ -102,8 +155,13 @@ const texts: MantraType[] = [
     दुर्गम काज जगत के जेते ।
     सुगम अनुग्रह तुम्हरे तेते ॥ २० ॥`,
     time: 113.6,
-    english: '',
-    meaning: '',
+    english: `Prabhu Mudrika Meli Mukha Maaheen । Jaladhi Langhi Gaye Acharaj Naheen
+    Durgam Kaaj Jagat Ke Jeete । Sugam Anugrah Tumhre Te Te.`,
+    meaning:  `Carrying Lord Sri Rama's Ring in your Mouth,
+    You Crossed the Ocean, no Wonder in that.
+    All the Difficult Tasks in this World,
+    Are Rendered Easy by your Grace.
+    `,
   },
   {
     mantra: `राम-दुआरे तुम रखवारे ।
@@ -111,17 +169,27 @@ const texts: MantraType[] = [
     सब सुख लहै तुम्हारी सरना ।
     तुम रक्षक काहू को डर ना ॥ २२ ॥`,
     time: 123.7,
-    english: '',
-    meaning: '',
+    english: `Ram Duware Tum Rakhavare । Hot Na Aagya Binu Paisare
+    Sab Sukh Lahen Tumhari Sarna । Tum Rakshak Kaahu Ko Darnaa`,
+    meaning:  `You are the Gate-Keeper of Sri Rama's Kingdom.
+    No one can Enter without Your Permission.
+    Those who take Refuge in You enjoy all Happiness.
+    If You are the Protector, what is there to Fear?
+    `,
   },
   {
-    mantra: `आपन तेज सम्हारो आपे ।
+    mantra: `आपन तेज सम्हारो आपै ।
     तीनौं लोक हाँक ते काँपे ॥ २३ ॥
     भूत पिसाच निकट नहिं आवै ।
     महाबीर जब नाम सुनावै ॥ २४ ॥`,
     time: 133.8,
-    english: '',
-    meaning: '',
+    english: `Aapan Tej Samharo Aapei । Tinau Lok Hank Te Kanpei
+    Bhoot Pisaach Nikat Nahi Avei । Mahabir Jab Naam Sunavei.`,
+    meaning:  `You alone can Control Your Great Energy.
+    When you Roar, the Three Worlds Tremble.
+    Ghosts and Evil Spirits will Not Come Near,
+    When one Utters the Name of Mahavir (Hanuman).
+    `,
   },
   {
     mantra: `नासै रोग हरै सब पीरा ।
@@ -129,17 +197,25 @@ const texts: MantraType[] = [
     संकट तें हनुमान छुड़ावै ।
     मन क्रम बचन ध्यान जो लावै ॥ २६ ॥`,
     time: 144,
-    english: '',
-    meaning: '',
+    english: `Nasei Rog Hare Sab Peera । Japat Niranter Hanumant Beera
+    Sankat Te Hanuman Chhudavei । Man Kram Bachan Dhyan Jo Lavei.`,
+    meaning:  `You Destroy Diseases and Remove all Pains,
+    When one Utters your Name Continuously.
+    Samkatt Se Hanumaan Chuddaavai |
+    Man Kram Bacan Dhyaan Jo Laavai ||`,
   },
   {
-    mantra: `सब पर राम राय सिर ताजा।
-    तिन के काज सकल तुम साजा ॥ २७ ॥
+    mantra: `सब पर राम तपस्वी राजा ।
+    तिन के काज सकल तुम साजा।। ॥ २७ ॥
     और मनोरथ जो कोइ लावै ।
     सोई अमित जीवन फल पावै ॥ २८ ॥`,
     time: 154,
-    english: '',
-    meaning: '',
+    english: `Sub Par Ram Tapasvee Raaja । Tinke Kaaj Sakal Tum Saaja
+    Aur Manorath Jo Koi Lave । Soi Amit Jivan Phal Pave.`,
+    meaning:  `Sri Rama is the King of the Tapaswis (devotees engaged in penances).
+    And You (Hanuman) Fulfill all Works of Sri Rama (as a caretaker).
+    Devotees who have any Other Desires,
+    Will ultimately get the Highest Fruit of Life.`,
   },
   {
     mantra: `चारों जुग परताप तुम्हारा ।
@@ -147,26 +223,40 @@ const texts: MantraType[] = [
     साधु संत के तुम रखवारे ।
     असुर-निकंदन राम-दुलारे ॥ ३० ॥`,
     time: 164.1,
-    english: '',
-    meaning: '',
+    english: `Charo Juung Partap Tumhara । Hai Parsiddha Jagat Ujiyara.
+    Sadho Sant Ke Tum Rakhvare । Asur Nikandan Ram Dulare.`,
+    meaning:  `Your Glory prevails in all the Four Ages.
+    And your Fame Radiates throughout the World.
+    You are the Saviour of the Saints and Sages.
+    You Destroy the Demons, O Beloved of Sri Rama.`,
   },
   {
     mantra: `अष्ट सिद्धि नव निधि के दाता ।
     अस बर दीन्ह जानकी माता ॥ ३१ ॥
     राम-रसायन तुम्हरे पासा ।
-    सादर हो रघुपति के दासा ॥ ३२ ॥`,
+    सदा रहो रघुपति के दासा ॥ ३२ ॥`,
     time: 174.2,
-    english: '',
-    meaning: '',
+    english: `Ashta Siddhi Nau Nidhi Ke Data । Asa Bar Din Janki Mata
+    Ram Rasayan Tumhare Pasa । Sadaa Raho Raghupati Ke Dasa.`,
+    meaning:  `You can Give the Eight Siddhis (supernatural powers) and Nine Nidhis (types of devotions).
+    Mother Janaki (Devi Sita) gave this Blessing to you.
+    You hold the Essence of Devotion to Sri Rama.
+    You Always Remain as the Servant of Raghupati (Sri Rama).
+    `,
   },
   {
     mantra: `तुम्हरे भजन राम को पावै ।
     जनम जनम के दुख बिसरावै ॥ ३३ ॥
     अंत-काल रघुबर-पुर जाई ।
-    जहाँ जन्म हरि-भगत कहाई ॥ ३४ ॥`,
+    जहाँ जन्म हरिभक्त कहाई ॥ ३४ ॥`,
     time: 184.3,
-    english: '',
-    meaning: '',
+    english: `Tumhare Bhajan Ramko Pavei । Janam Janam Ke Dukh Bisravei
+    Anta Kaal Raghubar Pur Jai । Jahan Janma Hari Bhakta Kahai`,
+    meaning:  `Through Devotion to You, one gets Sri Rama,
+    Thereby getting Free of the Sorrows of Life after Life.
+    At the End one Goes to the Abode of Raghupati (Sri Rama).
+    Where one is Known as the Devotee of Hari.
+    `,
   },
   {
     mantra: `और देवता चित्त न धरई ।
@@ -174,17 +264,27 @@ const texts: MantraType[] = [
     संकट कटै मिटै सब पीरा ।
     जो सुमिरै हनुमत बलबीरा ॥ ३६ ॥`,
     time: 194.6,
-    english: '',
-    meaning: '',
+    english: `Aur Devata Chitt Na Dharai । Hanumant Sei Sarva Sukh Karai
+    Sankat Kate Mitey Sab Peera । Jo Sumirei Hanumant Balbeera`,
+    meaning:  `Even without Worshipping any Other Deities,
+    One Gets All Happiness who Worships Sri Hanuman.
+    Difficulties Disappear and Sorrows are Removed,
+    For Those who Contemplate on the Powerful Sri Hanuman.
+    `,
   },
   {
     mantra: `जय जय जय हनुमान गोसाईं ।
     कृपा करहु गुरुदेव की नाईं ॥ ३७ ॥
-    यह सत बार पाठ कर कोई ।
+    जो शत बार पाठ कर कोई ।
     छूटहिं बंदि महा सुख होई ॥ ३८ ॥`,
     time: 204.7,
-    english: '',
-    meaning: '',
+    english: `Jai Jai Jai Hanuman Gosai । Kripa Karahu Gurudev Ki Naiee
+    Jo Sat Baar Paath Kar Koi । Chhutahi Bandi Maha Sukh Hoi.`,
+    meaning:  `Victory, Victory, Victory to You, O Hanuman,
+    Please Bestow your Grace as our Supreme Guru.
+    Those who Recite this Hanuman Chalisa one hundred times (with devotion),
+    Will get Freed from Worldly Bondage and get Great Happiness.
+    `,
   },
   {
     mantra: `जो यह पढ़ै हनुमान-चलीसा ।
@@ -192,8 +292,13 @@ const texts: MantraType[] = [
     तुलसीदास सदा हरि-चेरा ।
     कीजै नाथ हृदय महँ डेरा ॥ ४० ॥`,
     time: 214.7,
-    english: '',
-    meaning: '',
+    english: `Jo Yah Padhe Hanuman Chalisa । Hoy Siddhi Sakhi Gaurisa
+    Tulsidas Sada Hari Chera । Keeje Nath Hriday Mah Dera`,
+    meaning:  `Those who Read the Hanuman Chalisa (with devotion),
+    Will become Perfect, Lord Shiva is the Witness.
+    Tulsidas who is Always the Servant of Hari.
+    Prays the Lord to Reside in his Heart.
+    `,
   },
   {
     mantra: `पवनतनय संकट-हरन,
@@ -203,8 +308,11 @@ const texts: MantraType[] = [
     सियावर रामचंद्र की जय ।
     पवनसुत हनुमान की जय ।`,
     time: 224.8,
-    english: '',
-    meaning: '',
+    english: `Chopai : Pavan Tanay Sankat Haran, Mangal Murti Roop, Ram Lakhan Sita Sahit, Hriday Basahu Sur Bhoop`,
+    meaning:  `Sri Hanuman, who is the Son of Pavana, who Removes Difficulties,
+    Who has an Auspicious Form,
+    With Sri Rama, Sri Lakshmana and Devi Sita,
+    Please Dwell in my Heart.`,
   },
 ];
 
