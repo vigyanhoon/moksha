b285225be572252261f2dc2fcc44d5a378dc3c21
sanjay.pandey <vigyanhoon@yahoo.co.in>
2026-06-27 11:39:49 +0530

    add shiv panchakshar stotra

--- web/src/mantraConfig.ts
+++ web/src/mantraConfig.ts
@@ -2,11 +2,13 @@ import vishnuSahasranamaTexts from "./texts/vishnu_sahasranama.js";
 import mahisasurMardiniTexts from "./texts/mahisasur_mardini.js";
 import hanumanChalisaTexts from "./texts/hanuman_chalisa.js";
 import shivTandatStrotaTexts from "./texts/shiv_tandav_strota.js";
+import shivPanchaksharStotra from "./texts/shiv_panchakshar_stotra.js";
 
 import vishnuSahasranamaAudio from "./audio/vishnu_sahasranama.mp3";
 import mahisasurMardiniAudio from "./audio/mahisasur_mardini.mp3";
 import hanumanChalisaAudio from "./audio/hanuman_chalisa.mp3";
 import shivTandatStrotaAudio from "./audio/shiv_tandav_strota.mp3";
+import shivPanchaksharStotraAudio from "./audio/shiv_panchakshar_stotra.mp3";
 
 export const mantraConfig = [
     {
@@ -36,6 +38,13 @@ export const mantraConfig = [
         path: "ShivTandavStrota",
         text: shivTandatStrotaTexts,
         audio: shivTandatStrotaAudio,
+    },
+    {
+        name: "Shiv Panchakshar Strota",
+        resource: "shiv_panchakshar_strota",
+        path: "ShivPanchaksharStrota",
+        text: shivPanchaksharStotra,
+        audio: shivPanchaksharStotraAudio,
     },
 ] as const;
 
